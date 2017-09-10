18cf6b9c9e398b760cc91913610f15e1d307cd1f
Xin Wang <wangxin.wang@gmail.com>
2017-09-10 13:48:50 +0800

    Keep the toolbar always displayed. Add first map

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,20 @@
 
 $("#closeBtn").click(function() {
-    $("#leftPanel").addClass("hide");
+  $("#leftPanel").addClass("hide");
 });
 
 $("#menuBtn").click(function() {
-    $("#leftPanel").removeClass("hide");
+  $("#leftPanel").removeClass("hide");
 });
+
+function initMap() {
+  var seattle = {lat: 47.610984, lng: -122.337031};
+  var map = new google.maps.Map(document.getElementById('mapContainer'), {
+    zoom: 13,
+    center: seattle
+  });
+  var marker = new google.maps.Marker({
+    position: seattle,
+    map: map
+  });
+}
